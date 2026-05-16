f8661fe14b34f9315a207414f12cd7dcc24852e2
jakedoesdev <jakedoesdev@gmail.com>
2026-05-16 15:56:17 -0400

    delete script

--- tmp.py
+++
@@ -1,6 +0,0 @@
-import json, sys
-for line in sys.stdin:
-    h = json.loads(line)
-    if 'github' in h['fqdn'] and h['scope']['status'] == 'out':
-        print(h['fqdn'], h['scope']['matched_rule'])
-        break
